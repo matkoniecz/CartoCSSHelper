f3b1764bb57e2b04b5b0ba7e4f0ffccfe3433799
Mateusz Konieczny <matkoniecz@gmail.com>
2014-10-31 08:48:55 +0100

    handle NOT IN in tag_hunter

--- tag_hunter.rb
+++ tag_hunter.rb
@@ -25,11 +25,11 @@ def get_tags
 	for element in matched
 		tags.add([element[2], element[4]])
 	end
-	matched = yaml.scan(/("|)([^"() ]+)("|) IN \(([^()]*)\)/)
+	matched = yaml.scan(/("|)([^"() ]+)("|) (NOT |)IN \(([^()]*)\)/)
 	for element in matched
 		tag = element[1]
 		tag = tag.gsub(/.*\./, "")
-		values = element[3]
+		values = element[4]
 		values_matched = values.scan(/'([^']+)'/)
 		for value in values_matched
 			tags.add([tag, value[0]])
