944483b2fc4b76394dd04943770eeea02ce88069
Mateusz Konieczny <matkoniecz@gmail.com>
2014-10-31 08:33:32 +0100

    expand tag_hunter

--- tag_hunter.rb
+++ tag_hunter.rb
@@ -12,5 +12,28 @@ def get_tags
 			tags.add([element[0], element[1]])
 		end
 	end
-	return tags
+	yaml_filename = get_style_path+'project.yaml'
+	yaml_file = open(yaml_filename)
+	yaml = yaml_file.read()
+	#(.*\.|)     #WHERE p.highway = 'turning_circle'
+	#("|)        #natural and other SQL keywords
+	#([^"() ]+)  #tag
+	#repeat
+	# = 
+	#'([^'()]+)' #quoted value
+	matched = yaml.scan(/(.*\.|)("|)([^"() ]+)("|) = '([^'()]+)'/)
+	for element in matched
+		tags.add([element[2], element[4]])
+	end
+	matched = yaml.scan(/("|)([^"() ]+)("|) IN \(([^()]*)\)/)
+	for element in matched
+		tag = element[1]
+		tag = tag.gsub(/.*\./, "")
+		values = element[3]
+		values_matched = values.scan(/'([^']+)'/)
+		for value in values_matched
+			tags.add([tag, value[0]])
+		end
+	end
+	return tags.to_a.sort
 end
