c1cdfbd98bde22fdd1f88e939b0a3b79fbbe7925
Mateusz Konieczny <matkoniecz@gmail.com>
2014-12-04 10:11:57 +0100

    folder was moved

--- config.rb
+++ config.rb
@@ -4,9 +4,9 @@ def get_max_z
 end
 
 def get_style_path
-	return ENV['HOME']+'/Desktop/OSM/tilemill/osm-carto/'
+	return ENV['HOME']+'/Desktop/OSM/tilemill/osm-carto/' #TODO - stop hardcoding this
 end
 
 def get_path_to_folder_for_temporary_files
-	return ENV['HOME']+'/Desktop/OSM/tilemill/cmp/tmp/'
+	return ENV['HOME']+'/Desktop/OSM/tilemill/CartoCSSHelper/tmp/' #TODO - stop hardcoding this
 end
