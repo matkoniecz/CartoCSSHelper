4dc5d70e444995769e481bf15116125e651036ec
Mateusz Konieczny <matkoniecz@gmail.com>
2014-12-07 11:07:27 +0100

    mark variables as unused

--- test.rb
+++ test.rb
@@ -6,7 +6,7 @@ load 'tag_lister.rb'
 def main
 	Dir.chdir(get_style_path) {
 		require 'open3'
-		Open3.popen3('git log -n 1 --pretty=format:"%H"') {|i, stdout, stderr, t|
+		Open3.popen3('git log -n 1 --pretty=format:"%H"') {|_, stdout, stderr, _|
 			$commit = stdout.read.chomp
 			error = stderr.read.chomp
 			if error != ''
