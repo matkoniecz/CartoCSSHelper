81f9d3739b50ccb5030f8839d7004dafe58f7a0a
Mateusz Konieczny <matkoniecz@gmail.com>
2014-12-07 17:48:21 +0100

    autocreate necessary folders

--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
 # encoding: UTF-8
+require 'fileutils'
+
 def get_max_z
 	return 22
 end
@@ -8,5 +10,8 @@ def get_style_path
 end
 
 def get_path_to_folder_for_temporary_files
-	return ENV['HOME']+"/Documents/OSM/tilemill/CartoCSSHelper/tmp/#{$commit}/" #TODO - stop hardcoding this
+	#TODO - stop hardcoding this
+	location = ENV['HOME']+"/Documents/OSM/tilemill/CartoCSSHelper/tmp/#{$commit}/"
+	FileUtils::mkdir_p location
+	return location
 end
