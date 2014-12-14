a7d72eede296f42dfe45eb699b518f15aafaf719
Mateusz Konieczny <matkoniecz@gmail.com>
2014-12-14 12:33:33 +0100

    improve comments about composites

--- tag_lister.rb
+++ tag_lister.rb
@@ -116,15 +116,15 @@ class Info
 			return nil
 		end
 		composite_sets = [
-				{'name' => 'a'}, #place=*
+				{'name' => 'a'}, #place=city...
 				{'highway' => 'service'}, #access, ref, bridge, tunnel, service=parking_aisle...
 				{'railway' => 'rail'}, #service=siding
 				{'boundary' => 'administrative'}, #admin_level
 				{'admin_level' => '2'}, #boundary=administrative
-				{'natural' => 'peak'}, #ele
+				{'natural' => 'peak'}, #ele=*
 				{'ref' => '3'}, #aeroway=gate
 				{'amenity' => 'place_of_worship'}, #religion
-				{'amenity' => 'place_of_worship', 'religion' => 'christian'}, #denomination
+				{'amenity' => 'place_of_worship', 'religion' => 'christian'}, #denomination=jehovahs_witness
 				{'waterway' => 'river'}, #bridge=aqueduct, tunnel=culvert
 				#{'barrier' => 'hedge'}, #area=yes
 		]
