b6997e909e372543c6349658d07df71d33af309d
Mateusz Konieczny <matkoniecz@gmail.com>
2014-12-14 19:15:56 +0100

    better examples

--- test.rb
+++ test.rb
@@ -8,10 +8,10 @@ require 'open3'
 include Validator
 
 def main
-	visualise_changes({'highway' => 'motorway', 'name' => 'name', 'ref' => '1'}, 'way', false, 4..22, 'master', 'master')
-	visualise_changes({'amenity' => 'parking', 'name' => 'name'}, 'node', false, 4..22, 'master', 'master')
-	#visualise_changes({'amenity' => 'parking', 'name' => 'name'}, 'closed_way', false, 4..22, 'master', 'master')
-	#visualise_changes({'amenity' => 'parking', 'name' => 'name'}, 'way', false, 4..22, 'master', 'master')
+	#visualise_changes({'man_made' => 'power_wind', 'name' => 'ÉÉÉÉÉÉ ÉÉÉÉÉÉ ÉÉÉÉÉÉ', 'ref' => '1', 'ele' => '8000'}, 'node', false, 4..22, 'master', 'deprecated')
+	#visualise_changes({'amenity' => 'parking', 'name' => 'ÉÉÉÉÉÉ ÉÉÉÉÉÉ ÉÉÉÉÉÉ', 'ref' => '1', 'ele' => '8000'}, 'closed_way', false, 4..22, 'master', 'master')
+	#visualise_changes({'amenity' => 'parking', 'name' => 'ÉÉÉÉÉÉ ÉÉÉÉÉÉ ÉÉÉÉÉÉ', 'ref' => '1', 'ele' => '8000'}, 'way', false, 4..22, 'master', 'master')
+	switch_to_branch 'master'
 	init_commit_hash
 	info = Info.new
 	info.list_render_state_of_tags
