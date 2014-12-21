ec172fdecd7f1123426639120c78d8d8a98a46bd
Mateusz Konieczny <matkoniecz@gmail.com>
2014-12-21 20:42:45 +0100

    use File.join

--- configuration.rb
+++ configuration.rb
@@ -12,12 +12,12 @@ module Configuration
 	end
 
 	def get_style_path
-		return ENV['HOME']+'/Documents/OSM/tilemill/osm-carto/' #TODO - stop hardcoding this
+		return File.join(ENV['HOME'], 'Documents', 'OSM', 'tilemill', 'osm-carto', '') #TODO - stop hardcoding this
 	end
 
 	def get_path_to_folder_for_temporary_files
 		#TODO - stop hardcoding this
-		location = ENV['HOME']+"/Documents/OSM/tilemill/CartoCSSHelper/tmp/#{$commit}/"
+		location = File.join(ENV['HOME'], 'Documents', 'OSM', 'tilemill', 'CartoCSSHelper', 'tmp', $commit, '')
 		FileUtils::mkdir_p location
 		return location
 	end
