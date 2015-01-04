f56293de997492605543de9d86d999634d48c459
Mateusz Konieczny <matkoniecz@gmail.com>
2015-01-04 11:11:34 +0100

    -joke

--- test.rb
+++ test.rb
@@ -10,7 +10,7 @@ include Validator
 def wrapper(main_key, main_value, variable_key, variable_key_values, type, branch_from, branch_to)
 	tags = {main_key => main_value, 'name' => 'ÉÉÉÉÉÉ ÉÉÉÉÉÉ ÉÉÉÉÉÉ', 'ref' => '1', 'ele' => '8000'}
 	visualise_changes(tags, type, false, Configuration.get_min_z..Configuration.get_max_z, branch_from, branch_to)
-	variable_key_values.add('trololo')
+	variable_key_values.add('*')
 	variable_key_values.each { |value|
 		tags = {main_key => main_value, variable_key => value, 'name' => 'ÉÉÉÉÉÉ ÉÉÉÉÉÉ ÉÉÉÉÉÉ', 'ref' => '1', 'ele' => '8000'}
 		visualise_changes(tags, type, false, Configuration.get_min_z..Configuration.get_max_z, branch_from, branch_to)
