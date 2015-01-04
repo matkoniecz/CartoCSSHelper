078abd3be6668ed1a96a66fc30f74773dd680bc2
Mateusz Konieczny <matkoniecz@gmail.com>
2015-01-04 08:58:29 +0100

    wrapper to visualise changes to reduce boilerplate code

--- test.rb
+++ test.rb
@@ -7,10 +7,18 @@ load 'git.rb'
 require 'open3'
 include Validator
 
+def wrapper(main_key, main_value, variable_key, variable_key_values, type, branch_from, branch_to)
+	tags = {main_key => main_value, 'name' => 'ÉÉÉÉÉÉ ÉÉÉÉÉÉ ÉÉÉÉÉÉ', 'ref' => '1', 'ele' => '8000'}
+	visualise_changes(tags, type, false, Configuration.get_min_z..Configuration.get_max_z, branch_from, branch_to)
+	variable_key_values.add('trololo')
+	variable_key_values.each { |value|
+		tags = {main_key => main_value, variable_key => value, 'name' => 'ÉÉÉÉÉÉ ÉÉÉÉÉÉ ÉÉÉÉÉÉ', 'ref' => '1', 'ele' => '8000'}
+		visualise_changes(tags, type, false, Configuration.get_min_z..Configuration.get_max_z, branch_from, branch_to)
+	}
+end
 def main
-	#visualise_changes({'man_made' => 'power_wind', 'name' => 'ÉÉÉÉÉÉ ÉÉÉÉÉÉ ÉÉÉÉÉÉ', 'ref' => '1', 'ele' => '8000'}, 'node', false, 4..22, 'master', 'deprecated')
-	#visualise_changes({'amenity' => 'parking', 'name' => 'ÉÉÉÉÉÉ ÉÉÉÉÉÉ ÉÉÉÉÉÉ', 'ref' => '1', 'ele' => '8000'}, 'closed_way', false, 4..22, 'master', 'master')
-	#visualise_changes({'amenity' => 'parking', 'name' => 'ÉÉÉÉÉÉ ÉÉÉÉÉÉ ÉÉÉÉÉÉ', 'ref' => '1', 'ele' => '8000'}, 'way', false, 4..22, 'master', 'master')
+	wrapper('leisure', 'playground', 'access', Set.new(['private', 'yes']), 'closed_way', 'master', 'private_transparent')
+	wrapper('amenity', 'recycling', 'access', Set.new(['private', 'yes']), 'closed_way', 'master', 'private_transparent')
 	switch_to_branch 'master'
 	init_commit_hash
 	info = Info.new
