c03ff739972102aa552ee14e8524c098d3677bbc
Mateusz Konieczny <matkoniecz@gmail.com>
2015-03-21 20:40:34 +0100

    code more resistant to copy-paste

--- CartoCSSHelper.rb
+++ CartoCSSHelper.rb
@@ -17,29 +17,29 @@ module CartoCSSHelper
     #special support for following tag values:  :any_value
     krakow_latitude = 50.1
     krakow_longitude = 19.9
-    VisualDiff.visualise_changes_on_real_data(tags, type, krakow_latitude, krakow_longitude, zlevels, old_branch, new_branch)
+    CartoCSSHelper::VisualDiff.visualise_changes_on_real_data(tags, type, krakow_latitude, krakow_longitude, zlevels, old_branch, new_branch)
     japan_latitude = 36.1
     japan_longitude = 140.7
-    VisualDiff.visualise_changes_on_real_data(tags, type, japan_latitude, japan_longitude, zlevels, old_branch, new_branch)
+    CartoCSSHelper::VisualDiff.visualise_changes_on_real_data(tags, type, japan_latitude, japan_longitude, zlevels, old_branch, new_branch)
     russia_latitude = 54.8
     russia_longitude = 31.7
-    VisualDiff.visualise_changes_on_real_data(tags, type, russia_latitude, russia_longitude, zlevels, old_branch, new_branch)
+    CartoCSSHelper::VisualDiff.visualise_changes_on_real_data(tags, type, russia_latitude, russia_longitude, zlevels, old_branch, new_branch)
     mecca_latitude = 21.3
     mecca_longitude = 39.5
-    VisualDiff.visualise_changes_on_real_data(tags, type, mecca_latitude, mecca_longitude, zlevels, old_branch, new_branch)
+    CartoCSSHelper::VisualDiff.visualise_changes_on_real_data(tags, type, mecca_latitude, mecca_longitude, zlevels, old_branch, new_branch)
     georgia_latitude = 41.4
     georgia_longitude = 44.5
-    VisualDiff.visualise_changes_on_real_data(tags, type, georgia_latitude, georgia_longitude, zlevels, old_branch, new_branch)
+    CartoCSSHelper::VisualDiff.visualise_changes_on_real_data(tags, type, georgia_latitude, georgia_longitude, zlevels, old_branch, new_branch)
     london_latitude = 51.5
     london_longitude = -0.1
-    VisualDiff.visualise_changes_on_real_data(tags, type, london_latitude, london_longitude, zlevels, old_branch, new_branch)
+    CartoCSSHelper::VisualDiff.visualise_changes_on_real_data(tags, type, london_latitude, london_longitude, zlevels, old_branch, new_branch)
     #TODO: solve problems with respons to big to store in memory
     #utrecht_latitude =  52.09
     #utrecht_longitude = 5.11
-    #VisualDiff.visualise_changes_on_real_data(tags, type, utrecht_latitude, utrecht_longitude, zlevels, old_branch, new_branch)
+    #CartoCSSHelper::VisualDiff.visualise_changes_on_real_data(tags, type, utrecht_latitude, utrecht_longitude, zlevels, old_branch, new_branch)
     rural_uk_latitude =  53.2
     rural_uk_longitude = -1.8
-    VisualDiff.visualise_changes_on_real_data(tags, type, rural_uk_latitude, rural_uk_longitude, zlevels, old_branch, new_branch)
+    CartoCSSHelper::VisualDiff.visualise_changes_on_real_data(tags, type, rural_uk_latitude, rural_uk_longitude, zlevels, old_branch, new_branch)
   end
 
   def self.add_common_secondary_tags(tags)
@@ -57,5 +57,4 @@ module CartoCSSHelper
     syn_tags = add_common_secondary_tags(tags)
     VisualDiff.visualise_changes_synthethic_test(syn_tags, type, test_on_water, zlevels, old_brach, new_branch)
   end
-
 end
