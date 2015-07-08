0a5a2adaf52073e50cb19f19db1b3f5d5f45878c
Mateusz Konieczny <matkoniecz@gmail.com>
2015-07-08 13:34:49 +0200

    new convenience functions

--- lib/cartocss_helper.rb
+++ lib/cartocss_helper.rb
@@ -68,4 +68,49 @@ module CartoCSSHelper
       CartoCSSHelper::VisualDiff.visualise_changes_synthethic_test(syn_tags, type, test_on_water, zlevels, old_brach, new_branch)
     }
   end
+
+  def visualise_place_by_url(url, zlevels, new_branch, old_branch='master', header=nil, bb=0.04, image_size = 450)
+    latitude = url.scan(/\/((-|)\d+(\.\d+))/)[0][0].to_f
+    longitude = url.scan(/\/((-|)\d+(\.\d+))/)[1][0].to_f
+    if header == nil
+      header = url
+    end
+    header += ' ' + old_branch + '->' + new_branch + ' ' + zlevels.to_s + ' '+ image_size.to_s + 'px'
+    CartoCSSHelper::VisualDiff.visualise_changes_for_location(latitude, longitude, zlevels, header, old_branch, new_branch, bb, image_size)
+  end
+
+  def get_place_of_storage_of_resource_under_url(url)
+    return CartoCSSHelper::Configuration.get_path_to_folder_for_overpass_cache + '.manual.cache' + FileHelper::make_string_usable_as_filename(url)
+  end
+
+  def download_remote_file(url)
+    filename = get_place_of_storage_of_resource_under_url(url)
+    if !File.exists?(filename)
+      begin
+        url = url
+        timeout = 600
+        data = RestClient::Request.execute(:method => :get, :url => url, :timeout => timeout)
+      rescue => e
+        puts "visualise_place_by_remote_file failed to fetch #{url}"
+        raise e
+      end
+      file = File.new(filename, 'w')
+      file.write data
+      file.close
+    end
+  end
+
+  def visualise_place_by_remote_file(url, latitude, longitude, zlevels, new_branch, old_branch='master', header=nil, bb=0.04, image_size = 700)
+    download_remote_file(url)
+    filename = get_place_of_storage_of_resource_under_url(url)
+    visualise_place_by_file(filename, latitude, longitude, zlevels, new_branch, old_branch, header, bb, image_size)
+  end
+
+  def visualise_place_by_file(filename, latitude, longitude, zlevels, new_branch, old_branch='master', header=nil, bb=0.04, image_size = 700)
+    if header == nil
+      header = filename
+    end
+    header += ' ' + old_branch + '->' + new_branch + '[' + latitude.to_s + ',' + longitude.to_s + ']' + ' ' + image_size.to_s + 'px'
+    CartoCSSHelper::VisualDiff.visualise_changes_for_location_from_file(filename, latitude, longitude, zlevels, header, old_branch, new_branch, bb, image_size)
+  end
 end
