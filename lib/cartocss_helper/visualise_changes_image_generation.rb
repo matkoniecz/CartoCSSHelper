e726b8e84c715f60e819142e33c001215ba72623
Mateusz Konieczny <matkoniecz@gmail.com>
2015-07-15 09:07:00 +0200

    return info whatever image was generated

--- lib/cartocss_helper/visualise_changes_image_generation.rb
+++ lib/cartocss_helper/visualise_changes_image_generation.rb
@@ -145,11 +145,12 @@ module CartoCSSHelper
         latitude, longitude = Downloader.locate_element_with_given_tags_and_type tags, type, wanted_latitude, wanted_longitude
       rescue Downloader::OverpassRefusedResponse
         puts 'No nearby instances of tags and tag is not extremely rare - no generation of nearby location and wordwide search was impossible. No diff image will be generated for this location.'
-        return
+        return false
       end
       header = "#{ VisualDiff.dict_to_pretty_tag_list(tags) } #{type} #{ wanted_latitude } #{ wanted_longitude } #{zlevels}"
       download_bbox_size = 0.4
       visualise_changes_for_location(latitude, longitude, zlevels, header, new_branch, old_branch, download_bbox_size)
+      return true
     end
 
     def self.visualise_changes_for_location(latitude, longitude, zlevels, header, new_branch, old_branch, download_bbox_size, image_size=400)
