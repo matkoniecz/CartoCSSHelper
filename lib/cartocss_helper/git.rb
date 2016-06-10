140e39bbccb8ea94933fbe4b53873296e34c70b6
Mateusz Konieczny <matkoniecz@gmail.com>
2016-06-10 23:45:27 +0200

    pass debug status

--- lib/cartocss_helper/git.rb
+++ lib/cartocss_helper/git.rb
@@ -10,7 +10,7 @@ module CartoCSSHelper
         require 'open3'
         command = "git checkout #{branch}"
         begin
-          execute_command(command, ignore_stderr_presence: true)
+          execute_command(command, debug, ignore_stderr_presence: true)
         rescue FailedCommandException => e
           raise 'failed checkout to ' + branch + ' due to ' + e
         end
