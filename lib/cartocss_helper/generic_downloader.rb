a653923a2eb1140e147e3ead520561a850cf6ee0
Mateusz Konieczny <matkoniecz@gmail.com>
2016-06-11 00:08:35 +0200

    simplify code in rest-client handler

--- lib/cartocss_helper/generic_downloader.rb
+++ lib/cartocss_helper/generic_downloader.rb
@@ -1,6 +1,9 @@
 require 'ruby-progressbar'
 
 class GenericDownloader
+  class ResourcePernamentlyUnavailable < StandardError
+  end
+
   def initialize(timeout: 20, error_message: nil, additional_lethal: [])
     @timeout = timeout
     @retry_count = 0
@@ -33,33 +36,34 @@ class GenericDownloader
     if retry_count > retry_max || @lethal_exceptions.include?(exception.class)
       raise exception
     end
+    wait
     @retry_count += 1
   end
 
   def get_specified_resource(url)
-    data = RestClient::Request.execute(method: :get, url: url, timeout: timeout)
-    # see https://github.com/rest-client/rest-client/issues/370, will be fixed in 2.0
-    # published versions listed on https://rubygems.org/gems/rest-client/versions/
-    return String.new(data)
+    return fetch_data_from_url_using_rest_client(url)
   rescue SocketError, URI::InvalidURIError => e
     output_shared_error_part(error_message, url, e)
-    wait
     get_specified_resource(url) if retry_allowed(e)
   rescue RestClient::RequestFailed, RestClient::ServerBrokeConnection, RestClient::ResourceNotFound => e
     output_shared_error_part(error_message, url, e)
     puts e.response
     puts e.http_code
-    wait
     get_specified_resource(url) if retry_allowed(e)
   rescue ArgumentError => e
     output_shared_error_part(error_message, url, e)
-    puts 'ArgumentError from rest-client, most likely caused by https://github.com/rest-client/rest-client/issues/359'
-    raise e
+    raise ResourcePernamentlyUnavailable, 'ArgumentError from rest-client, most likely caused by https://github.com/rest-client/rest-client/issues/359'
   rescue => e
     puts 'unhandled exception! It is a clear bug!'
     output_shared_error_part(error_message, url, e)
-    wait
     puts "<#{e.class} error happened>"
     raise e
+  end
+
+  def fetch_data_from_url_using_rest_client(url)
+    data = RestClient::Request.execute(method: :get, url: url, timeout: timeout)
+    # see https://github.com/rest-client/rest-client/issues/370, will be fixed in 2.0
+    # published versions listed on https://rubygems.org/gems/rest-client/versions/
+    return String.new(data)
   end
 end
