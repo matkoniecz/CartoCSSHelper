bfb1600b4a9ade69ab7ec6bc964b7c3598f658b2
Mateusz Konieczny <matkoniecz@gmail.com>
2016-06-12 06:38:06 +0200

    more git

--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -1,16 +1,26 @@
 require_relative '../lib/cartocss_helper.rb'
 require_relative '../lib/cartocss_helper/git.rb'
 require 'spec_helper'
- 
+
 describe CartoCSSHelper::Git do
-	it "should issue correct command" do
-		#TODO: why Git is nested in CartoCSSHelper?
-    allow(SystemHelper).to receive(:execute_command).and_raise(FailedCommandException)
+  it "what about failed chdir" do
     path_to_project = "aajaajajjaj"
     allow(CartoCSSHelper::Configuration).to receive(:get_path_to_tilemill_project_folder).and_return(path_to_project)
+
+    # see http://ruby-doc.org/core-2.1.1/Dir.html#method-c-chdir
+    allow(Dir).to receive(:chdir).and_raise(Errno::ENOENT)
+    expect { CartoCSSHelper::Git.checkout("branch") }.to raise_error FailedCommandException
+  end
+
+  it "should issue correct command" do
+    path_to_project = "aajaajajjaj"
+    allow(CartoCSSHelper::Configuration).to receive(:get_path_to_tilemill_project_folder).and_return(path_to_project)
+
+    # TODO: why Git is nested in CartoCSSHelper?
+    allow(SystemHelper).to receive(:execute_command).and_raise(FailedCommandException)
     allow(Dir).to receive(:chdir) do |block|
-    	yield(block)
+      yield(block)
     end
-    expect { CartoCSSHelper::Git.checkout("branch") }.to raise_error FailedCommandException 
-	end	
+    expect { CartoCSSHelper::Git.checkout("branch") }.to raise_error FailedCommandException
+  end
 end
