c356fa5526f972d6375a9a4370032d06aabc465c
Mateusz Konieczny <matkoniecz@gmail.com>
2016-06-13 19:48:47 +0200

    one more test for systemhelper

--- spec/systemhelper_spec.rb
+++ spec/systemhelper_spec.rb
@@ -14,8 +14,11 @@ describe SystemHelper do
   it "should raise exception on stderr" do
     expect { SystemHelper.execute_command(">&2 echo error") }.to raise_error FailedCommandException
   end
-  it "should raise exception error code" do
+  it "should raise exception on an error code" do
     expect { SystemHelper.execute_command("exit 700") }.to raise_error FailedCommandException
+  end
+  it "should not raise exception on a success code" do
+    expect(SystemHelper.execute_command("exit 0")).to eq ""
   end
   it "should not raise exception on ignored stderr" do
     expect(SystemHelper.execute_command(">&2 echo error", ignore_stderr_presence: true)).to eq "error"
