9e3694485ea2fc74b5dc9a03e813da671f43a51d
Mateusz Konieczny <matkoniecz@gmail.com>
2016-07-24 17:18:01 +0200

    DRY - new function, dropped duplicate

--- lib/cartocss_helper/notes_downloader.rb
+++ lib/cartocss_helper/notes_downloader.rb
@@ -2,21 +2,23 @@ require_relative 'util/generic_cached_downloader.rb'
 
 module CartoCSSHelper
   class NotesDownloader
+    def self.cache_filename(lat, lon, range)
+      url = NotesDownloader.format_query_into_url(lat, lon, range)
+      return CartoCSSHelper::Configuration.get_path_to_folder_for_notes_api_cache + url.delete("/") + ".cache"
+    end
+
     def self.run_note_query(lat, lon, range, invalidate_cache: false)
       timeout = NotesDownloader.get_allowed_timeout_in_seconds
       downloader = GenericCachedDownloader.new(timeout: timeout, stop_on_timeout: false)
-
+      file = cache_filename(lat, lon, range)
       url = NotesDownloader.format_query_into_url(lat, lon, range)
-      cache_filename = CartoCSSHelper::Configuration.get_path_to_folder_for_notes_api_cache + url.delete("/") + ".cache"
-      return downloader.get_specified_resource(url, cache_filename, invalidate_cache: invalidate_cache)
+      return downloader.get_specified_resource(url, file, invalidate_cache: invalidate_cache)
     end
 
     def self.cache_timestamp(lat, lon, range)
       downloader = GenericCachedDownloader.new
-
-      url = NotesDownloader.format_query_into_url(lat, lon, range)
-      cache_filename = CartoCSSHelper::Configuration.get_path_to_folder_for_notes_api_cache + url.delete("/") + ".cache"
-      return downloader.get_cache_timestamp(cache_filename)
+      file = cache_filename(lat, lon, range)
+      return downloader.get_cache_timestamp(file)
     end
 
     def self.get_allowed_timeout_in_seconds
