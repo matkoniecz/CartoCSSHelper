cac6666156b4bfa47a2e519897c2395b8b6b1274
Mateusz Konieczny <matkoniecz@gmail.com>
2018-01-07 18:29:45 +0100

    make tests more readable as recommended by rubocop

--- spec/overpass_query_generator_spec.rb
+++ spec/overpass_query_generator_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 
 describe CartoCSSHelper::OverpassQueryGenerator do
   it "should handle spaces in tags" do
-    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([['a','b']])).to eq "\t['a'='b']\n"
-    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([['a','b b']])).to eq "\t['a'='b b']\n"
-    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([['a  ',' b b ']])).to eq "\t['a  '=' b b ']\n"
+    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([['a', 'b']])).to eq "\t['a'='b']\n"
+    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([['a', 'b b']])).to eq "\t['a'='b b']\n"
+    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([['a  ', ' b b ']])).to eq "\t['a  '=' b b ']\n"
   end
   it "should handle quotes in tags" do
-    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([['a"','"""']])).to eq "\t['a\"'='\"\"\"']\n"
-    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([["'","''"]])).to eq "\t['\\''='\\'\\'']\n"
+    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([['a"', '"""']])).to eq "\t['a\"'='\"\"\"']\n"
+    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([["'", "''"]])).to eq "\t['\\''='\\'\\'']\n"
   end
   it "should handle backslashes in tags" do
-    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([["surface","wood\\"]])).to eq "\t['surface'='wood\\\\']\n"
+    expect(CartoCSSHelper::OverpassQueryGenerator.turn_list_of_tags_in_overpass_filter([["surface", "wood\\"]])).to eq "\t['surface'='wood\\\\']\n"
   end
 end
