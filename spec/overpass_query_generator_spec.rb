a02840037db536f34435ce755abe4ccd508d1305
Mateusz Konieczny <matkoniecz@gmail.com>
2021-04-08 11:35:44 +0200

    document overpass escaping a bit

--- spec/overpass_query_generator_spec.rb
+++ spec/overpass_query_generator_spec.rb
@@ -28,4 +28,17 @@ describe CartoCSSHelper::OverpassQueryGenerator do
   it "can accept negations" do
     expect(CartoCSSHelper::OverpassQueryGenerator.get_query_element_to_get_location({'surface' => {:operation => :not_equal_to, :value => 'wood'}}, 10, 10, 'node', 100)).to eq "(node['surface'!='wood'](around:100,10,10););\n\n"
   end
+  it "will not escape what needs no escape" do
+    expect(CartoCSSHelper::OverpassQueryGenerator.overpass_escape('surface')).to eq "surface"
+  end
+  it "double quotes will not be escaped as values in a query is surrounded by single quotes" do
+    expect(CartoCSSHelper::OverpassQueryGenerator.overpass_escape('"')).to eq '"'
+  end
+  it "will escape quoyes" do
+    backslash = "\\"
+    expected = backslash+"'"
+    puts(expected.length)
+    puts(CartoCSSHelper::OverpassQueryGenerator.overpass_escape("'").length)
+    expect(CartoCSSHelper::OverpassQueryGenerator.overpass_escape("'")).to eq expected
+  end
 end
